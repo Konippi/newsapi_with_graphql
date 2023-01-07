fff0d8bbfb6fbeea0231e767a4fcb2808fc59265
Konippi <konippi-114@outlook.jp>
2023-01-07 16:04:29 +0900

    available sqlite using prisma

--- src/server.js
+++ src/server.js
@@ -1,39 +1,35 @@
 const { ApolloServer, gql } = require("apollo-server");
 const fs = require("fs");
 const path = require("path");
+const { PrismaClient } = require("@prisma/client")
 
-// hacker news
-let links = [
-    {
-        id: "link-0",
-        description: "tutorial",
-        url: "https://www.tutorial.com"
-    }
-];
+const prisma = new PrismaClient();
 
 // resolver func
 const resolvers = {
     Query: {
         info: () => "HackerNews",
-        feed: () => links,
+        feed: async (parent, args, context) => context.prisma.link.findMany(),
     },
     Mutation: {
-        post: (parent, args) => {
-            let idCount = links.length;
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url,
-            };
-            links.push(link);
-            return link;
-        }
+        post: (parent, args, context) => {
+            const newLink = context.prisma.link.create({
+                data: {
+                    url: args.url,
+                    description: args.description,
+                },
+            });
+            return newLink;
+        },
     },
 };
 
 const server = new ApolloServer({
     typeDefs: fs.readFileSync(path.join(__dirname, "graphql/schema.graphql"), "utf-8"),
     resolvers,
+    context: {
+        prisma,
+    },
 });
 
 server.listen().then(({ url }) => console.log(`${url}で起動中!`));
